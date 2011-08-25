4e3da15336ea7ef60c81f86c3f58da06853c84c5
Pedro Salgado <pedro.salgado@1on1.com>
2011-08-25 09:32:29 -0600

    added documentation and minor format changes.

--- test/unit/cfg/manager_test.rb
+++ test/unit/cfg/manager_test.rb
@@ -3,49 +3,45 @@ require 'lib/steenzout-cfg'
 
 
 
+# Test cases for Steenzout::ConfigurationManager.
+#
 class TestConfigurationManager < Test::Unit::TestCase
 
-  def setup
-
-    Steenzout::ConfigurationManager.load 'test/simple.yaml'
-
+  def setup()
+   Steenzout::ConfigurationManager.load 'test/simple.yaml'
   end
 
 
 
-  def test_configuration_for_gem
-
+  def test_configuration_for_gem()
     config = Steenzout::ConfigurationManager.configuration_for_gem 'steenzout-cfg'
     assert_equal({:property => 'value1'}, config)
-
   end
 
 
 
-  def test_load
-
+  def test_load()
     # 1. loading another file with same gem configuration
     Steenzout::ConfigurationManager.load 'test/another.yaml'
-
+    
     config = Steenzout::ConfigurationManager.configuration_for_gem 'steenzout-cfg'
     assert_equal({:property => 'value2'}, config)
-
-
+    
+    
     # 2. loading another file with no duplicate configuration
     Steenzout::ConfigurationManager.load 'test/yetanother.yaml'
-
+    
     config = Steenzout::ConfigurationManager.configuration_for_gem 'steenzout-cfg'
     assert_equal({:property => 'value2'}, config)
-
+    
     config = Steenzout::ConfigurationManager.configuration_for_gem 'steenzout-dao'
     assert_equal({:property => 'value3'}, config)
-
-
+    
+    
     # 3. loading from inexistent file
     assert_raises ArgumentError do
       Steenzout::ConfigurationManager.load 'test/inexistent.yaml'
     end
-
   end
 
 end
